84749f3151ed40d77bb08a220a20ecca86cfa25c
wangxueze <xueze.wang@edreadminfo.com>
2020-05-27 14:34:53 +0800

    IU-2020.1.1 <wang@DESKTOP-914QRA0 Create baseRefactoring.xml, markdown.xml, ide.general.xml, git.xml, templateLanguages.xml, project.default.xml, Default copy.xml, freemarker.ftl.ftl, terminal.xml, Default.xml, ActionScript File Header.as, find.xml, ui.lnf.xml, filetypes.xml, projectView.xml, keymap.xml, colors.scheme.xml, mavenVersion.xml, templates.xml, abbreviations.xml, Default.xml, laf.xml, Class.java, jdk.table.xml, Template.xml, path.macros.xml, toStringTemplates.xml, mybatis.xml, editor.xml, usageView.xml, debugger.xml, updates.xml

---
+++ fileTemplates/includes/ActionScript File Header.as
@@ -0,0 +1,9 @@
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈${DESCRIPTION}〉
+ *
+ * @Author: wangxueze
+ * @Description:
+ * @Date: Created in ${TIME} ${DATE}
+ * @Modified By:
+ */
---
+++ fileTemplates/internal/Class.java
@@ -0,0 +1,12 @@
+#if (${PACKAGE_NAME} && ${PACKAGE_NAME} != "")package ${PACKAGE_NAME};#end
+#parse("File Header.java")
+
+/**
+ * 〈一句话功能简述〉<br> 
+ * 〈${DESCRIPTION}〉
+ *
+ * @author ${USER}
+ * @create ${DATE}
+ */
+public class ${NAME} {
+}
